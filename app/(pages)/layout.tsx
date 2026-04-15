099beadfa658df322e3c19ccb7dff096d28f3e54
Hasch <haschdevv@gmail.com>
2026-04-16 00:24:01 +0800

    Fixed yaml file

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import '@/app/globals.css'
-import { SidebarProvider, SidebarTrigger, SidebarInset } from '@/components/ui/sidebar';
-import { AppSidebar } from "@/components/ui/app-sidebar"
 import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
 
@@ -41,22 +39,13 @@ export default function RootLayout({
   return (
     <div lang="en">
       <div className="font-sans antialiased">
-        <SidebarProvider defaultOpen={false}>
-          <AppSidebar />
-
-          <SidebarInset className="flex flex-col min-h-screen">
-            {/* TOP BAR */}
             <div className="p-4 top-0 left-0 right-0 z-10 bg-transparent w-auto">
-              <SidebarTrigger />
+
                 <div className="flex-1">
                     {children}
                 </div>
             </div>
-            {/* PAGE CONTENT */}
-            {/* FOOTER */}
             <Footer />
-          </SidebarInset>
-        </SidebarProvider>
         {process.env.NODE_ENV === 'production' && <Analytics />}
       </div>
     </div>
